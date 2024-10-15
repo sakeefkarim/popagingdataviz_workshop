a5f14d3c3b2b5e8d5cc8405a9a649150bd9e6081
Sakeef Karim <sakeefkarim@Sakeefs-MacBook-Pro-2.local>
2024-10-15 18:05:44 -0400

    Adding global package list

---
+++ code/packages.R
@@ -0,0 +1,101 @@
+# ------------------------------------------------------------------------------
+
+# PopAging DataViz
+# Sakeef M. Karim
+# List of PACKAGES
+# CC-BY-SA 4.0
+
+
+# DAY 1 -------------------------------------------------------------------
+
+library(palmerpenguins)
+library(gapminder)
+library(demography)
+library(covdata)
+library(cansim)
+library(WDI)
+library(LexisPlotR)
+
+# Themes, Colour Schemes
+
+library(ggthemes)
+library(hrbrthemes)
+library(see)
+library(paletteer)
+library(gglgbtq)
+library(colorspace)
+
+# Additional packages, geoms, tools
+
+library(lemon)
+library(skimr)
+library(lattice)
+library(ggrepel)
+library(ggridges)
+library(ggtext)
+library(ggdist)
+library(LexisPlotR)
+
+
+# DAY 2 -------------------------------------------------------------------
+
+library(systemfonts)
+library(tidyverse)
+
+# Mapping in ggplot2
+
+library(sf)
+library(sp)
+library(terra)
+library(ggspatial)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(ggthemes)
+library(tidygeocoder)
+library(geojsonsf)
+library(tidycensus)
+library(cancensus)
+library(mapcan)
+library(usmap)
+library(usmapdata)
+library(mapview)
+library(leaflet)
+library(leafpop)
+
+# Animations
+
+library(plotly)
+library(gganimate)
+library(ggiraph)
+
+# Misc
+
+library(patchwork)
+library(see)
+library(scales)
+library(ggrepel)
+
+
+# DAY 3 -------------------------------------------------------------------
+
+library(tidyverse)
+library(systemfonts)
+
+
+# Packages for estimating models, visualizing results:
+
+library(estimatr)
+library(ggeffects)
+library(marginaleffects)
+library(modelsummary)
+library(effects)
+
+# Misc:
+
+library(gtsummary)
+library(gt)
+library(ggthemes)
+library(hrbrthemes)
+library(gssr)
+library(panelr)
+library(skimr)
